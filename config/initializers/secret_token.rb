abaa5c094ad906d7ab02d41601d0465f6024c6df
D. Love <d.d.love20@gmail.com>
2012-10-17 22:55:43 -0700

    Creating rails fake social app and added status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '29bd530bf50af62d0d6a1240e625f64e212200eabb55eaeb825f54e14b3183b121e47bb2c869fc3c229c59e8a6da1c6a501defdcd794f1445374ea736fa78a5d'
