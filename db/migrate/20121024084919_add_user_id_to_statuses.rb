6682e8e7bbe1c98477fe618518ccc0df061a6e85
D. Love <d.d.love20@gmail.com>
2012-10-24 01:56:42 -0700

    installed views for devise add association of user id to statuses

---
+++ db/migrate/20121024084919_add_user_id_to_statuses.rb
@@ -0,0 +1,8 @@
+class AddUserIdToStatuses < ActiveRecord::Migration
+  def change
+    add_column :statuses, :user_id, :integer
+    add_index :statuses, :user_id
+    remove_column :statuses, :name
+    
+  end
+end
