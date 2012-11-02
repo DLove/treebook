3e2f9d59c307a9a67cf4522a5a04ec0d20e03792
D. Love <d.d.love20@gmail.com>
2012-11-01 21:11:55 -0700

    tried writing some test to validate user sign up

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test " a user should enter a first name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test " a user should enter a last name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test " a user should enter a profile name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+  	user = User.new
+  	user.email = "d.d.love20@gmail.com"
+  	user.profile_name = users(:dlove).profile_name
+  	
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
 end
