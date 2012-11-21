3b7e0513e6367f691aec3196cc502ac40e0816da
D. Love <d.d.love20@gmail.com>
2012-11-21 13:28:20 -0800

    updated routes to all to get toe a profile page, added statues to show up on profile page

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -2,8 +2,20 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:dlove).profile_name
     assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test "should render a 404 on profile not found"  do
+  	get :show, id: "doesn't exist"
+  	assert_response :not_found
+  end
+
+  test "that variables are assigned on successful profile viewing" do
+  	get :show, id: users(:dlove).profile_name
+  	assert assigns(:user)
+  	assert_not_empty assigns(:statuses)
   end
 
 end
